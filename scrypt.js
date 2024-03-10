4dcf3c368e200b0ca0a350400382ef0186ba038f
Luis Gómez <luisvillagesell@gmail.com>
2024-03-10 20:31:17 -0300

    se agrega forII

--- scrypt.js
+++ scrypt.js
@@ -2,7 +2,8 @@
 var container = document.getElementById('box-container');
 
 // Definimos la cantidad de cajas que queremos generar
-var numBoxes = 10;
+var numBoxes = 20;
+
 
 // Creamos un bucle for para generar las cajas
 for (var i = 1; i <= numBoxes; i++) {
@@ -11,7 +12,29 @@ var box = document.createElement('div');
 // Le asignamos una clase para darle estilo con CSS
 box.classList.add('box');
 // Le asignamos un texto con el número de caja
-box.textContent = 'Box ' + i;
+box.textContent = 'Alumno' + i;
 // Añadimos la caja al contenedor
 container.appendChild(box);
+}
+
+console.log('hola mundo')
+
+let contador = 0;
+
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+
+do {
+    console.log(contador);
+    contador++;
+} while (contador < 10)
+
+/*
+Cuando ejecutemos este script,
+}    */
+
+for (let i = 0; i < 10; i++) {
+    console.log('For ' + i);
 }
